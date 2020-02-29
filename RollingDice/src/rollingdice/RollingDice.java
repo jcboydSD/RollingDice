763cb28fc42e5f18d3fcee7b3dd4e6c15ce7907c
JCBoyd <jcboyd@pm.me>
2020-02-29 08:11:34 -0700

    Added even or odd method to display sum and message

--- RollingDice/src/rollingdice/RollingDice.java
+++ RollingDice/src/rollingdice/RollingDice.java
@@ -16,7 +16,7 @@ public class RollingDice
         Scanner stdIn = new Scanner(System.in);
         //Declarations
         int die1, die2; //values of two dice
-        String playAgainAnswer = "y"; //default is to play again
+        String playAgainAnswer = ""; //default is to play again
 
         //Input, Processing, Output
         System.out.println("Roll Dice by JC Boyd\n");
@@ -24,14 +24,14 @@ public class RollingDice
 
         do
         {
-            System.out.print("Press enter to roll the dice:");
+            System.out.print("Press enter to roll the dice ");
             stdIn.nextLine();
             die1 = rollDie(); //call method to generate random number
             die2 = rollDie(); //call method to generate random number
             printResult(die1, die2); //call method to display messages  
             System.out.print("Do you want to play again? (y/n) ");
             playAgainAnswer = stdIn.nextLine();
-        } while (!playAgainAnswer.equals("n")); // end do-while
+        } while (!playAgainAnswer.equals("n"));
 
     } //end main
     
@@ -46,16 +46,33 @@ public class RollingDice
     
     public static void printResult(int die1, int die2) //Method displays dice 
     {                                                  //values and if doubles 
+        System.out.printf("You rolled a %d and a %d.\n", die1, die2);
+        evenOdd(die1, die2); //call method to sum and print even or odd msg
+        
         if (die1 == die2)
         {
-            System.out.printf("Doubles! You rolled two %d's."
-                    + " Thank you for playing!\n", die1);
+            System.out.println("Doubles! Thank you for playing!");
         }
         else
         {
-            System.out.printf("No doubles. You rolled a %d and a %d." 
-                    + " Try again.\n", die1, die2);
+            System.out.println("No doubles. Try again.");
         }
     } //end printResult
+
+    //*************************************************************************
+    
+    public static void evenOdd(int die1, int die2) //Method displays even or odd 
+    {                                              //message based on sum 
+        int dieSum = die1 + die2;
         
+        if (dieSum %2 == 0) //check for even number (no remainder)
+        {
+            System.out.printf("The total is the even number %d.\n", dieSum);
+        }
+        else
+        {
+            System.out.printf("The total is the odd number %d.\n", dieSum);
+        }
+    } //end evenOdd
+    
 } //end class RollingDice
